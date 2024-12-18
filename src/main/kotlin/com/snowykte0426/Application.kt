f8a789fa27af64efa7f4e1873cc3267d4575f946
snowykte0426 <snowykte0426@naver.com>
2024-12-18 09:02:50 +0900

    라우팅 및 정적 리소스 설정

---
+++ src/main/kotlin/com/snowykte0426/Application.kt
@@ -0,0 +1,17 @@
+package com.snowykte0426
+
+import com.example.configureRouting
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.routing.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+    routing {
+        staticResources("/static", "static")
+    }
+}
