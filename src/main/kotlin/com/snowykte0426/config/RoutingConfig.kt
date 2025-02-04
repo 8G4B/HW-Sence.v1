27faaf06cee487a7e05da8a5cc1bd1be93b7fc6d
snowykte0426 <snowykte0426@naver.com>
2025-02-04 18:01:34 +0900

    ``staticResources`` 함수 적용법 변경

--- src/main/kotlin/com/snowykte0426/config/RoutingConfig.kt
+++ src/main/kotlin/com/snowykte0426/config/RoutingConfig.kt
@@ -1,21 +1,23 @@
 package com.snowykte0426.config
 
-import io.ktor.server.application.*
-import io.ktor.server.routing.*
-import io.ktor.server.http.content.*
-import io.ktor.server.html.respondHtml
 import com.snowykte0426.service.fetchSystemStatus
 import com.snowykte0426.ui.generateHtmlPage
+import io.ktor.server.application.*
+import io.ktor.server.html.*
+import io.ktor.server.http.content.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         route("/hw-sence") {
-            staticResources("/static", "static")
             get("/status") {
                 val status = fetchSystemStatus()
                 call.respondHtml {
                     generateHtmlPage(status)
                 }
+            }
+            route("/status") {
+                staticResources("/static", "static")
             }
         }
     }
